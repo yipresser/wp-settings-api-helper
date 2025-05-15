bdb3f5f420a962b240b6d172dd79eb67d581c80e
Damien Oh <damien.ohwj@gmail.com>
2025-05-15 12:46:39 +0800

    changed version to 1.0.1.4

--- src/wp-settings-api-helper.php
+++ src/wp-settings-api-helper.php
@@ -4,7 +4,7 @@ namespace Yipresser\WpSettingsApiHelper;
 /**
  * Yipresser WP Settings API Helper abstract class
  *
- * @version 1.0.1.3
+ * @version 1.0.1.4
  *
  * @author Damien Oh <damien@yipresser.com>
  */
